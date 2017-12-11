1cd6f205bcd52e28de7e040a9bae057a5b5458dd
uniqueshiv <uniqueshivchauhan@gmail.com>
2017-12-11 12:42:29 +0530

    method chaining exmaple add

--- method-chaining.php
+++ method-chaining.php
@@ -1,4 +1,30 @@
 <?php
+// class ABC{
+//   public $value='$value porperties of ABC Class';
+// }
+// class XYZ{
+//   public $abc;
+//   public function __construct(){
+//     $this->abc=new ABC;
+//   }
+// }
+// $xyz=new XYZ;
+// echo $xyz->abc->value;
 
 
+class ABC{
+  public function fun1(){
+    echo "fun1 of ABC class";
+    return $this;
+  }
+  public function fun2(){
+    echo "fun2 of ABC Class";
+    return $this;
+  }
+  public function fun3(){
+    echo "fun3 of ABC Class";
+  }
+}
+$abc =new ABC;
+$abc->fun1()->fun2()->fun3();
 ?>
