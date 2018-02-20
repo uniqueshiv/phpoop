3110cb2bae1c8c35788ec49e5d5e390139a09c83
uniqueshiv <uniqueshivchauhan@gmail.com>
2018-02-20 16:27:48 +0530

    update

---
+++ string_function.php
@@ -0,0 +1,34 @@
+<?php
+$str = 'A quick brown fox jumps over the lazy dog';
+ $str1=234;
+
+ //echo strlen($str);//find string length
+
+
+ //  if(is_string($str1)){ // check is string or not
+ //     echo "yes string";
+ // }else{
+ //      echo "not string";
+ // }
+
+
+//echo str_word_count($str); // word count
+
+//echo strrev($str); //string reverse
+
+//substr(String,start,length) get substring from string
+//echo substr($str,0,10); //return a quick br
+//echo substr($str,5,10);//ck brown f
+//echo substr($str,-10); //return e lazy dog
+
+//echo substr($str,5,-10); //return ck brown fox jumps over th
+
+// echo strtoupper($str);
+//echo strtolower($str);
+
+echo trim($str);
+// echo strpos($str,'a');
+
+// $files = glob('*.php');
+//print_r($files);
+ s
